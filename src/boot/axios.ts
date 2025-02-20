62c438ac89c8ed1bde4bd425c25acbdecd91882b
Everton Reis <joaoeverton2015@gmail.com>
2025-02-19 21:47:45 -0300

    feat: serviços de cadastro e processo com axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,4 +1,3 @@
-import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
 
 declare module '@vue/runtime-core' {
@@ -8,24 +7,8 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const cadastroService = axios.create({ baseURL: '/cadastro-service' });
+const processoService = axios.create({ baseURL: '/processo-service' });
 
-export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
-});
-
-export { api };
+export { cadastroService, processoService };
